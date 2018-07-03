652fec71941457694f43a77b8a8e8fe12dbdb33f
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-07-03 14:35:04 -0400

    add  chat header component  in chat

--- src/Main.js
+++ src/Main.js
@@ -5,12 +5,19 @@ import Chat from './Chat'
 class Main extends Component{
     render(){
         return(
-<div className = "Main">
+<div style = {styles} className = "Main">
 <SideBar />
 <Chat  />
 </div>
         )
     }
+}
+
+
+const styles  = {
+ display: 'flex',
+ alignItems: 'stretch',
+ height: '100vh',
 }
 
 export default Main
